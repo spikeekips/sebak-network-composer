e872d0322d97298bfc5ccd809f09c3077389cc4d
Spike^ekipS <spikeekips@gmail.com>
2018-12-03 14:41:16 +0900

    update genesis balance to default

--- docker/entrypoint.from-source.sh
+++ docker/entrypoint.from-source.sh
@@ -8,7 +8,7 @@ export SEBAK_STORAGE="file:///tmp/db/${SEBAK_NODE_ALIAS}"
 env | sort
 
 cd /sebak
-go run cmd/sebak/main.go genesis ${SEBAK_GENESIS_BLOCK} ${SEBAK_COMMON_ACCOUNT} --balance 1000000000000000000 || true
+go run cmd/sebak/main.go genesis ${SEBAK_GENESIS_BLOCK} ${SEBAK_COMMON_ACCOUNT} || true
 
 go run cmd/sebak/main.go node \
     --network-id "${SEBAK_NETWORK_ID}" \
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,7 +7,7 @@ export SEBAK_STORAGE="file:///tmp/db/${SEBAK_NODE_ALIAS}"
 
 env | sort
 
-/sebak genesis ${SEBAK_GENESIS_BLOCK} ${SEBAK_COMMON_ACCOUNT} --balance 1000000000000000000 || true
+/sebak genesis ${SEBAK_GENESIS_BLOCK} ${SEBAK_COMMON_ACCOUNT} || true
 
 /sebak node \
     --network-id "${SEBAK_NETWORK_ID}" \
